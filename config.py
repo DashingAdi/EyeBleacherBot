a67b97e486028dbdba468b37ed4c8ee2d06d78aa
DashingAdi <60130665+DashingAdi@users.noreply.github.com>
2020-10-29 11:12:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-username = ""
-password = ""
-client_id = ""
-client_secret =  ""
+username = "No-Hat1243"
+password = "dashingadi"
+client_id = "iLtKGhpg-Ks8vw"
+client_secret =  "rLGl4u6MEatdsYLPswJwcJOujno"
 user_agent = ("Eye Bleacher Bot v1.0 by u/cyanidesuppository")
